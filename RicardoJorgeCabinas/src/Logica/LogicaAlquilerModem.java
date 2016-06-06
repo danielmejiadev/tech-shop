a3d24a2e8dd37ca5d24331bf4087fa0ef4dba17b
andresloaiza21 <andresloaiza21@gmail.com>
2016-06-06 12:32:24 -0500

    Modulo Alquiler USB-Modem sin promociones

--- RicardoJorgeCabinas/src/Logica/LogicaAlquilerModem.java
+++ RicardoJorgeCabinas/src/Logica/LogicaAlquilerModem.java
@@ -1,9 +1,17 @@
 package Logica;
 
+import Modelo.AlquilerModem;
+import Persistencia.AlquilerModemJpaController;
+
 /**
  *
  * @author Daniel
  */
 public class LogicaAlquilerModem {
+    AlquilerModemJpaController alquilerModemDAO = new AlquilerModemJpaController();
+    
+    public void registrarAlquilerModem(AlquilerModem alquilerModem){
+        alquilerModemDAO.create(alquilerModem);
+    }
     
 }
